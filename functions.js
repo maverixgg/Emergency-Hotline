7e853430692b17830278081e2649c6fe0bdbd162
maverixgg <fardinturjo7@gmail.com>
2025-09-22 21:44:49 +0600

    copy button finished

--- functions.js
+++ functions.js
@@ -1,4 +1,8 @@
 var loveCounter = 0;
+let coinCount = document.getElementById('coins')
+let callHistory = []
+let copynum = document.getElementById('copy-num')
+
 
 function addFavorite(id){
     const parent = document.getElementById(id)
@@ -16,50 +20,123 @@ function addFavorite(id){
     document.getElementById('heartCount').innerText = loveCounter;
 }
 
-document.getElementById('emergency-call').addEventListener('click', function(){
-    const serviceName = document.getElementById('emergency-service').innerText
-    const serviceNumber = document.getElementById('emergency-number').innerText
+function calling(id){
+    let currentCoin = parseInt(coinCount.innerText)
+    if(currentCoin>=20){
+        currentCoin -= 20
+        coinCount.innerText = currentCoin
+    }
+    else {
+        alert('Not Enough Coins')
+        return;
+    }
+    const serviceName = document.getElementById(`${id}-service`).innerText
+    const serviceNumber = document.getElementById(`${id}-number`).innerText
     alert(`${serviceName}: ${serviceNumber}`)
+
+    let time = new Date().toLocaleTimeString();
+    console.log(time)
+
+    const history = {
+        Service: serviceName,
+        Number: serviceNumber,
+        Time: time
+    }
+
+    callHistory.push(history)
+    console.log(callHistory)
+
+    const historyContainer = document.getElementById('history-container')
+    historyContainer.innerHTML = ''
+
+    for(let i of callHistory){
+        const div = document.createElement('div')
+        div.innerHTML = 
+        `
+        <div class="flex justify-between bg-[#FAFAFA] p-3 rounded-xl shadow-md my-3">
+                        <div>
+                            <h1>${i.Service}</h1>
+                            <h2>${i.Number}</h2>
+                        </div>
+                        <h3>${i.Time}</h3>
+        </div>
+        `
+        historyContainer.prepend(div)
+    }
+}
+
+function numCopy(id){
+    const num = document.getElementById(`${id}-number`).innerText
+    navigator.clipboard.writeText(num).then(() => {
+        alert("Number copied: " + num);
+      }).catch(err => {
+        console.error("Failed to copy: ", err);
+      });
+
+    let store = parseInt(copynum.innerText)
+    copynum.innerText = store+1
+}
+
+document.getElementById('emergency-copy').addEventListener('click', function(){
+    numCopy('emergency')
+})
+document.getElementById('police-copy').addEventListener('click', function(){
+    numCopy('police')
+})
+document.getElementById('fire-copy').addEventListener('click', function(){
+    numCopy('fire')
+})
+document.getElementById('ambulance-copy').addEventListener('click', function(){
+    numCopy('ambulance')
+})
+document.getElementById('woman-copy').addEventListener('click', function(){
+    numCopy('woman')
+})
+document.getElementById('eee-copy').addEventListener('click', function(){
+    numCopy('eee')
+})
+document.getElementById('rail-copy').addEventListener('click', function(){
+    numCopy('rail')
+})
+document.getElementById('brac-copy').addEventListener('click', function(){
+    numCopy('brac')
+})
+document.getElementById('gov-copy').addEventListener('click', function(){
+    numCopy('gov')
+})
+
+document.getElementById('clear-btn').addEventListener('click', function(){
+    const historyContainer = document.getElementById('history-container')
+    historyContainer.innerHTML = ''
+    historyContainer.innerText = 'No calls dialed'
+})
+
+document.getElementById('emergency-call').addEventListener('click', function(){
+    calling('emergency')
 })
 document.getElementById('police-call').addEventListener('click', function(){
-    const serviceName = document.getElementById('police-service').innerText
-    const serviceNumber = document.getElementById('police-number').innerText
-    alert(`${serviceName}: ${serviceNumber}`)
+    calling('police')
 })
 document.getElementById('fire-call').addEventListener('click', function(){
-    const serviceName = document.getElementById('fire-service').innerText
-    const serviceNumber = document.getElementById('fire-number').innerText
-    alert(`${serviceName}: ${serviceNumber}`)
+    calling('fire')
 })
 document.getElementById('woman-call').addEventListener('click', function(){
-    const serviceName = document.getElementById('woman-service').innerText
-    const serviceNumber = document.getElementById('woman-number').innerText
-    alert(`${serviceName}: ${serviceNumber}`)
+    calling('woman')
 })
 document.getElementById('ambulance-call').addEventListener('click', function(){
-    const serviceName = document.getElementById('ambulance-service').innerText
-    const serviceNumber = document.getElementById('ambulance-number').innerText
-    alert(`${serviceName}: ${serviceNumber}`)
+    calling('ambulance')
 })
 document.getElementById('gov-call').addEventListener('click', function(){
-    const serviceName = document.getElementById('gov-service').innerText
-    const serviceNumber = document.getElementById('gov-number').innerText
-    alert(`${serviceName}: ${serviceNumber}`)
+    calling('gov')
 })
 document.getElementById('eee-call').addEventListener('click', function(){
-    const serviceName = document.getElementById('eee-service').innerText
-    const serviceNumber = document.getElementById('eee-number').innerText
-    alert(`${serviceName}: ${serviceNumber}`)
+    calling('eee')
 })
 document.getElementById('brac-call').addEventListener('click', function(){
-    const serviceName = document.getElementById('brac-service').innerText
-    const serviceNumber = document.getElementById('brac-number').innerText
-    alert(`${serviceName}: ${serviceNumber}`)
+    calling('brac')
 })
 document.getElementById('rail-call').addEventListener('click', function(){
-    const serviceName = document.getElementById('rail-service').innerText
-    const serviceNumber = document.getElementById('rail-number').innerText
-    alert(`${serviceName}: ${serviceNumber}`)
+    calling('rail')
 })
 
 
